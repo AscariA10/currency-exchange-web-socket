81aad8dc39494a6feea228e5b186e0b03ede7f76
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2024-03-13 12:21:54 +0200

    1st commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.css";
+
+function App() {
+   return (
+      <>
+         <div className={styles.wrapper}>app css module test</div>
+      </>
+   );
+}
+
+export default App;
