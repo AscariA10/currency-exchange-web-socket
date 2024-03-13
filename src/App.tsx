247d4de095cd4f8e57cf089d7851f446224ca3cf
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2024-03-13 12:35:43 +0200

    add inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import styles from "./App.module.css";
+import styles from "./app.module.css";
 
 function App() {
    return (
       <>
-         <div className={styles.wrapper}>app css module test</div>
+         <div className={styles.wrapper}>
+            <input type="number" name="amount" />
+            <input type="radio" name="operation" value="buy" />
+            <input type="radio" name="operation" value="sell" />
+            <p>result:</p>
+         </div>
       </>
    );
 }
